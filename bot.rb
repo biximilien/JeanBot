42e4729a2d948a844a77de451799db32f4443a6b
biximilien <maxime.gauthier88@gmail.com>
2023-01-28 17:21:53 -0500

    feat: Display command usage
    Send command usage when it is not entered properly.

--- bot.rb
+++ bot.rb
@@ -29,21 +29,26 @@ bot.message do |event|
   $logger.info("Message from #{event.user.name} (#{event.user.id})")
   $logger.info(event.message.content)
 
-  if /(?<trigger>!moderation)\s(?<command>watchlist)(\s(?<subcommand>add|remove)\s<@(?<user_id>\d+)>)?/i =~ event.message.content
+  if /(?<trigger>!moderation)(\s(?<command>watchlist))?(\s(?<subcommand>add|remove)\s<@(?<user_id>\d+)>)?/i =~ event.message.content
     $logger.info("Moderation command from #{event.user.name} (#{event.user.id})")
     event.server.members.each do |member|
       # ensure triggering user has proper privileges
       if member.id == event.user.id && member.permission?(:administrator)
         # handle watchlist commands
-        if trigger && command == "watchlist" && subcommand.nil?
-          event.respond("Watch list: " + get_watch_list_users(event.server.id).map { |user_id| "<@#{user_id}>" }.join(", "))
-        elsif subcommand == "add" && user_id
-          add_user_to_watch_list(event.server.id, user_id.to_i)
-          event.respond("Added <@#{user_id}> to watch list")
-        elsif subcommand == "remove" && user_id
-          remove_user_from_watch_list(event.server.id, user_id.to_i)
-          event.respond("Removed <@#{user_id}> from watch list")
+        if trigger && command == "watchlist"
+          if subcommand.nil?
+            event.respond("Watch list: " + get_watch_list_users(event.server.id).map { |user_id| "<@#{user_id}>" }.join(", "))
+          elsif subcommand == "add" && user_id
+            add_user_to_watch_list(event.server.id, user_id.to_i)
+            event.respond("Added <@#{user_id}> to watch list")
+          elsif subcommand == "remove" && user_id
+            remove_user_from_watch_list(event.server.id, user_id.to_i)
+            event.respond("Removed <@#{user_id}> from watch list")
+          else
+            event.respond("Usage: !moderation watchlist [add|remove @user]")
+          end
         else
+          event.respond("Usage: !moderation watchlist [add|remove @user]")
         end
         # break early if we found the right user
         break
